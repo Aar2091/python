f9660ffa8a489a25295dc04d5cd9ec0663c478ea
Aar2091 <spheres-turbine-4t@icloud.com>
2025-02-23 11:30:13 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,12 @@
+tree1 = 99
+tree2 = 93
+tree3 = 87
+tree4 = 83
+tree5 = 50
+
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all 5 trees is:", sum)
+
+average = sum/5
+print("the average of all tres is:", average)
