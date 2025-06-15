7998f01f72178f0107cacd0ded685eb4f465b481
Aar2091 <spheres-turbine-4t@icloud.com>
2025-06-15 11:40:11 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime
+
+today =  date.today()
+
+now = datetime.now()
+
+print("Today's date is:", today)
+
+print("\nCurrent datw and time is:", now)
+
+print("\nDate comonents:", today.year, today.month, today.day)
